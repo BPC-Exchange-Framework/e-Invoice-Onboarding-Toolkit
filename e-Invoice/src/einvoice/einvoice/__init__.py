e3c465a347b13dcf3f77e853c135f32451215a97
Kelly Kinney <80925865+mnkellyk@users.noreply.github.com>
2021-06-29 18:50:19 -0500

    Upload eInvoice and associated files.
    Add e-invoice prototype

---
+++ e-Invoice/src/einvoice/einvoice/__init__.py
@@ -0,0 +1,29 @@
+#!/Users/kelly/Dev/virtualenvs/e-Invoice/bin python
+
+# Author: Kelly Kinney
+# Date: 2021-06-20 (June 20, 2021)
+# File: __inir__.py
+# About: implements einvoice to test
+# Notes: chmod 755 create_dev_struct.sh in order to execute.
+#
+# LICENSE
+# Copyright (C) 2021 Kelly Kinney
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# A copy of the GNU General Public License is included in the GitHub
+# repository root which contained this file.  
+# If not, see <http://www.gnu.org/licenses/>.
+
+import einvoice
+
+myInvoice = eInvoice()
+
