c6a78c3a58c1c7adb0e20150213d4e865c4172db
Kelly Kinney <mnkellyk@outlook.com>
2021-08-16 18:20:48 -0500

    Fixed app_handler.py issues

--- einvoice/einvoice/app_handler.py
+++ einvoice/einvoice/app_handler.py
@@ -10,22 +10,6 @@ This module is responsible for retaining the inovice while work is done to
 prepare the request do a UNAPTR DNS look-up to obtain the SMP URI, perform
 the UNAPTR DNS look-up and perform SMP query on the URI returned,
 
-    Args:
-        _party_id: str
-        The ique idefier tw the party being searched for.
-
-        _prty_idschema_type: str
-        An alternate Party ID schema if not using the default.schema
-
-        _einvoice: obj (einvoice)
-        An einvoice object (which is a JSON file)
-
-    Attributes:
-
-    Raises:
-
-    Returns:
-
 """
 import hashlib
 import base64
@@ -34,17 +18,19 @@ from einvoice.app_logging import create_logger
 from einvoice.urn import Urn
 
 
-class CreateUrn:
+class UrnHandler:
     """Constructs a base URN for the SML query and prepares the hashes.
 
     The base URN to be constructed as a string, and then hashed.
 
-    Ar:
+    Args:
 
-    Aibutetw
-        party_id_specification:twtr
+    Atributes:
+        log: object
+            A custom logging object.
+        specification: str
             The party ID specification.
-        party_id_schema_type: str
+        schema: str
             The party ID schema type.
         party_id: str
             The party ID
@@ -53,11 +39,36 @@ class CreateUrn:
             when called.
         final_urn: str
             A version of the full urn which is not constructed on the
-            fly but held (essentially as a constant)
-        urn_sha256_hash: str
-            The urn that has been hashed using the shaw256 hash.
-        urn_base32_hash: str
-            The urn that has been hashed a second time from shaw256 to base32.
+            fly but staticly (essentially as a constant)
+        pre_sha256_encoded_data: byte-like object
+            Conversion of the urn to a byte-like object to be conversted
+            to sha256
+        urn_sha256_hashed: object
+            The hashed sha256 version of the urn.
+        urn_readable_sha256_hash: str
+            The urn that has been hashed using the sha256 hash that has been
+            converted into something human readable via hexdigest()
+        urn_sha256_byte_obj: byte-like object
+            Encode the sha32 urn into a utf-8 byte-like object again
+            to prep for Base32 hash.
+        urn_b32_byte_obj: byte-like object
+            The Base32 hashed object which is still a "byte-like object."
+        urn_b32_hash_decoded: str
+            The final hash decoded from the byte-like object/utf-8 to a string.
+        urn_values: dictionary
+            A dictionary to hold values acculuated as we go along.
+        urn_formatter: str
+            Calls create_urn_lookup directly from within a class method to
+            obtain the string value of the urn.
+        urn_sha_256_applied: str
+            Calls method to apply sha256 hash directly from with the class
+            method to obtain a string value of the hashed value.
+        urn_b32_applied: str
+            The final output of the base32 applied to the sha256 and then
+            made human readable.
+        json_str: str
+            Holds urn_values for writing to json on the filesystem.
+
 
     Returns:
 
@@ -70,18 +81,20 @@ class CreateUrn:
     party_id = None
     urn = None
     final_urn = None
-    encoded_data = None
-    hash_256 = None
-    urn_sha_256_hash = None
-    b_string = None
-    b_string_base_32_hash = None
-    urn_sha_256_hash = None
-    urn_base32_hash = None
-
+    pre_sha256_encoded_data = None
+    urn_sha256_hashed = None
+    urn_readable_sha256_hash = None
+    urn_sha256_byte_obj = None
+    urn_b32_byte_obj = None
+    urn_b32_hash_decoded = None
+    urn_values = None
+    urn_formatter = None
+    urn_sha256_applied = None
+    urn_b32_applied = None
+    json_str = None
 
     def __init__(self):
         return None
-
 
     def create_urn_lookup(self, party_id):
         """Constructs the full URN for lookup"""
@@ -92,27 +105,29 @@ class CreateUrn:
         self.log.debug("Created urn: %s", self.final_urn)
         return self.final_urn
 
-    def apply_sha_256_hash(self, final_urn):
+    def apply_sha256_hash(self, final_urn):
         """Apply SHA256 hash to the lookup"""
         self.log.debug("Applying shaw256 hash.")
-        self.encoded_data = final_urn.encode()    # pylint disable=W0201
-        self.hash_256 = hashlib.sha256(self.encoded_data)  # pylint disable=W0201
-        self.urn_sha_256_hash = self.hash_256.hexdigest()   # pylint disable=W0201
-        self.log.debug("Hex version of shaw256 hash  is  %s",
-                       self.urn_sha_256_hash)
-        return self.urn_sha_256_hash
+        self.pre_sha256_encoded_data = final_urn.encode()
+        self.urn_sha256_hashed = hashlib.sha256(self.pre_sha256_encoded_data)
+        self.urn_readable_sha256_hash = self.urn_sha256_hashed.hexdigest()
+        self.log.debug(
+            "Hex version of shaw256 hash  is  %s",
+            self.urn_readable_sha256_hash
+        )
+        return self.urn_readable_sha256_hash
 
-    def apply_base_32_hash(self, urn_sha_256_hash):
-        """Apply Base32 encoding per the spec"""
-        self.log.debug("Applying Base32 to shaw256 hash.")
+    def apply_b32_hash(self, urn_readable_sha256_hash):
+        """Apply Base32 encoding to the SHA356 hash"""
+        self.log.debug("Applying Base32 to sha256 hash.")
         # first convert to a byte-like object
-        self.b_string = urn_sha_256_hash.encode("utf-8")
-        self.b_string_base_32_hash = base64.b32encode(self.b_string)
+        self.urn_sha256_byte_obj = urn_readable_sha256_hash.encode("utf-8")
+        self.urn_b32_byte_obj = base64.b32encode(self.urn_sha256_byte_obj)
         # Convert it back to a string so it can be handled by json
-        self.urn_base_32_hash = self.b_string_base_32_hash.decode("utf-8")
+        self.urn_b32_hash_decoded = self.urn_b32_byte_obj.decode("utf-8")
         self.log.debug("Base32 conversion of shaw256 is %s",
-                       self.urn_base32_hash)
-        return self.urn_base_32_hash
+                       self.urn_b32_hash_decoded)
+        return self.urn_b32_hash_decoded
 
     def write_urn_to_json(self, urn_dictionary, filename):
         """Write the urn values to a file"""
@@ -125,23 +140,23 @@ class CreateUrn:
     def meatgrinder(self, party_id):
         """Find the values of all steps necessary to prepare the urn lookup."""
         # Create a dictionary to hold the accumlated data points.
-        self.urn_values = {     # pylint disable=W0201
+        self.urn_values = {  # pylint disable=W0201
             "Party ID Specification": self.specification,
             "Party ID schema": self.schema,
             "Party ID": party_id,
         }
 
         # Construct the unencoded urn.
-        self.sml_lookup = self.create_urn_lookup(party_id)
-        self.urn_values["Base urn"] = self.sml_lookup   # pylint disable=W0201
+        self.urn_formatter = self.create_urn_lookup(party_id)
+        self.urn_values["Base urn"] = self.urn_formatter
 
         # apply the shaw256 hash to the urn
-        self.sml_lookup_sha_255_applied = self.apply_sha_256_hash(self.sml_lookup)  # pylint disable=W0201
-        self.urn_values["SHA256 Hashed urn"] = self.sml_lookup_sha_255_applied
+        self.urn_sha256_applied = self.apply_sha256_hash(self.urn_formatter)
+        self.urn_values["SHA256 Hashed urn"] = self.urn_sha256_applied
 
         # apply the base32 hash to the shaw256 hash
-        self.sml_lookup_256_to_b32 = self.apply_base_32_hash(self.ssml_lookup_sha_255_applied)  # pylint disable=W0201
-        self.urn_values["Base32 Hashed urn"] = self.sml_lookup_256_to_b32
+        self.urn_b32_applied = self.apply_b32_hash(self.urn_sha256_applied)
+        self.urn_values["Base32 Hashed urn"] = self.urn_b32_applied
 
         # write ths CreateSmlUrnclass CreateSmlUrn dataclass object to a file
         self.write_urn_to_json(self.urn_values, "./final_urn.json")
