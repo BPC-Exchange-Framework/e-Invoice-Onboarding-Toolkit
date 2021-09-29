5907b51047c8a84a89309874fcb2607fbe902755
mnkellyk <mnkellyk@outlook.com>
2021-09-29 15:06:30 -0500

    Artifact clean-up and latest certificate work.

--- einvoice/einvoice/app_listener.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-#
-# File: app_listener.py
-# About: Interface between web ui and ei_handler.  Callable as CLI.
-# Development: Kelly Kinney, Leo Rubiano
-# Date: 2021-07-16 (July 16th, 2021)
-#
--- einvoice/einvoice/http_ui.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-#
-# File: http_ui.py
-# About: Interface between UI and back-end processing.
-# Development: Kelly Kinney, Leo Rubiano
-# Date: 2021-07-16 (July 16th, 2021)
-#
