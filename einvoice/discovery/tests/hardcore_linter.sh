2f45ed2750a1a84ad8aec1e76fa2245f9ed67cae
Kelly Kinney <mnkellyk@outlook.com>
2022-02-06 14:12:13 -0600

    Restructured and renamed delivery and discovery as their own sub-packages.

--- einvoice/discovery/tests/hardcore_linter.sh
+++ einvoice/discovery/tests/hardcore_linter.sh
@@ -10,7 +10,7 @@ echo "Begin flake8 check..."
 # flake8
 flake8 ../conf/*.py
 flake8 ../data/*.py
-flake8 ./*.py
+flake8 -qq ./*.py
 flake8 ../*.py
 pylint ../*.py
 echo "End flake8 check..."
--- einvoice/discovery/tests/test_smp_query.py
+++ einvoice/discovery/tests/test_smp_query.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # pylint: disable=C0301
-# noqa: E501
+#
 # long lines of test data
 # File: test_smp_query.py
 # About: e-Invoice testing suite; smp_query.
@@ -8,30 +8,32 @@
 # Date: 2021-11-18 (November 18th, 2021)
 #
 """This is a test file to be run using pytest.
-
 """
 from einvoice.discovery.smp_query import SMPQuery
 
 
 def test_smp_query():
     """Test smp_query"""
-    urns = ('urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::'
-            'kellytestsmp',
-            'urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::'
-            'kelly2021',
-            'urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::'
-            'kellysmbtest',
-            'urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::'
-            'mnkellyk')
-    service_group_urls = ('https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellytestsmp',
-                          'https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akelly2021',
-                          'https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellysmbtest',
-                          'https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Amnkellyk')
+    urns = (
+        "urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::kellytestsmp",
+        "urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::kelly2021",
+        "urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::kellysmbtest",
+        "urn:oasis:names:tc:ebcore:partyid-type:iso6523:0088::mnkellyk",
+    )
 
-    service_urls = ('https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellytestsmp/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2',
-                    'https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akelly2021/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2',
-                    'https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellysmbtest/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2',
-                    'https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Amnkellyk/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2')
+    service_group_urls = (
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellytestsmp",    # flake8: noqa: E501
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akelly2021",    # flake8: noqa: E501
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellysmbtest",    # flake8: noqa: E501
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Amnkellyk",    # flake8: noqa: E501
+    )
+
+    service_urls = (
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellytestsmp/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2",    # flake8: noqa: E501
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akelly2021/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2",    # flake8: noqa: E501
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Akellysmbtest/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2",    # flake8: noqa: E501
+        "https://smp-api.sc-b2b.us/bdxr-smp-2/urn%3Aoasis%3Anames%3Atc%3Aebcore%3Apartyid-type%3Aiso6523%3A0088%3A%3Amnkellyk/services/bdx-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3AInvoice-1%3A%3AInvoice%23%23BPC-UBL-2.2",    # flake8: noqa: E501
+    )
 
     query = SMPQuery()
     for index, urn in enumerate(urns):
